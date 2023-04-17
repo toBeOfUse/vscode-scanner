192e5ec95d7dd7530954aa5c8f01f48d82b13c68
Mitch J <mjacovet@kent.edu>
2023-04-17 17:31:30 -0400

    make object oriented

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
-function getLineAttributes() {
-  const editorRegion = document.getElementById("workbench.parts.editor");
-  const lineNumber = editorRegion.querySelector(".line-numbers");
-  const lineNumberBox = lineNumber.getBoundingClientRect();
-  const editor = editorRegion.querySelector(".lines-content");
-  const editorBox = editor.getBoundingClientRect();
-  const line = editor.querySelector(".view-line").firstChild;
-  const lineBox = line.getBoundingClientRect();
-  const editorLeft = editorBox.left;
-  const lineHeight = lineNumberBox.height;
-  const charWidth = lineBox.width / line.innerText.length;
-  // assuming these will stay fairly constant
-  return {
-    editorLeft,
-    charWidth,
-    lineHeight,
-  };
-}
-
-let editor = getLineAttributes();
-
-function getEditorTop() {
-  const editorRegion = document.getElementById("workbench.parts.editor");
-  const editor = editorRegion.querySelector(".lines-content");
-  const editorBox = editor.getBoundingClientRect();
-  return editorBox.top;
-}
-
-function getRowCol(x, y) {
-  x /= window.devicePixelRatio;
-  y /= window.devicePixelRatio;
-  const editorTop = getEditorTop();
-  console.log(editorTop);
-  const col = (x - editor.editorLeft) / editor.charWidth;
-  const row = (y - editorTop) / editor.lineHeight;
-  return { row: Math.floor(row + 1), col: Math.floor(col + 1) };
-}
-
-if (window.ws) {
-  window.ws.close();
-}
-window.ws = new WebSocket("ws://localhost:8888/websocket/");
-window.ws.onmessage = function (evt) {
-  console.log(evt.data);
-  const coords = JSON.parse(evt.data);
-  if ("x" in coords && "y" in coords) {
-    const rc = getRowCol(coords.x, coords.y);
-    console.log(rc);
-    ws.send(JSON.stringify(rc));
+class CodePosServer {
+  static getEditorAttributes() {
+    const editorRegion = document.getElementById("workbench.parts.editor");
+    const lineNumber = editorRegion.querySelector(".line-numbers");
+    const lineNumberBox = lineNumber.getBoundingClientRect();
+    const editor = editorRegion.querySelector(".lines-content");
+    const editorBox = editor.getBoundingClientRect();
+    const line = editor.querySelector(".view-line").firstChild;
+    const lineBox = line.getBoundingClientRect();
+    const editorLeft = editorBox.left;
+    const editorTop = editorBox.top;
+    const lineHeight = lineNumberBox.height;
+    const charWidth = lineBox.width / line.innerText.length;
+    const openFile = editorRegion
+      .querySelector(".monaco-editor")
+      .getAttribute("data-uri");
+    // these could be cached and only updated when a MutationObserver fires
+    return {
+      editorLeft,
+      charWidth,
+      lineHeight,
+      editorTop,
+      openFile,
+    };
   }
-};
+
+  getRowCol(x, y) {
+    x /= window.devicePixelRatio;
+    y /= window.devicePixelRatio;
+    const editor = getEditorAttributes();
+    const col = (x - editor.editorLeft) / editor.charWidth;
+    const row = (y - editor.editorTop) / editor.lineHeight;
+    return {
+      row: Math.floor(row + 1),
+      col: Math.floor(col + 1),
+      file: editor.openFile,
+    };
+  }
+
+  static coordsCallback(websocketEvent) {
+    try {
+      const coords = JSON.parse(websocketEvent.data);
+      if ("x" in coords && "y" in coords) {
+        const rc = getRowCol(coords.x, coords.y);
+        console.log("got", websocketEvent, "sending", rc);
+        this.ws.send(JSON.stringify(rc));
+      } else {
+        throw "x and y not present";
+      }
+    } catch (e) {
+      console.error("Received unusable websocket message:");
+      console.error(websocketEvent);
+      console.error(e);
+    }
+  }
+
+  constructor() {
+    this.ws = new WebSocket("ws://localhost:8888/websocket/");
+    this.ws.onmessage = coordsCallback;
+    this.stop = () => this.ws.close();
+  }
+}
